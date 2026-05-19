4056fb136189faecd7f7bf8f79ad3e9616973549
IT25100050 <it25100050@my.sliit.lk>
2026-05-19 12:21:24 +0800

    Delete src/main/java/com/example/beautysalon/staff directory

--- src/main/java/com/example/beautysalon/staff/JuniorStylist.java
+++
@@ -1,7 +0,0 @@
-package com.project.beautysalon.model;
-
-public class JuniorStylist extends Stylist {
-    public JuniorStylist(Object o, String marcoDelacroix, String hairStyling, String mail, String s, String busy, double v, int i) {
-        super();
-    }
-}
--- src/main/java/com/example/beautysalon/staff/SeniorStylist.java
+++
@@ -1,7 +0,0 @@
-package com.project.beautysalon.model;
-
-public class SeniorStylist extends Stylist {
-    public SeniorStylist(Object o, String priyaNair, String nailTechnician, String mail, String s, String available, double v, int i) {
-        super();
-    }
-}
